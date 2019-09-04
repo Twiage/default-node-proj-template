44f080dd13f3008ab8cbabaa2c0a698c282681ba
Daniel Nomura <dnomura@twiagemed.com>
2019-09-04 16:33:23 +0200

    getting started with cdk

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,29 @@
+import cdk = require('@aws-cdk/core');
+import ec2 = require("@aws-cdk/aws-ec2");
+import ecs = require("@aws-cdk/aws-ecs");
+import ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(
+        this,
+
+    );
+
+    const cluster = new ecs.Cluster(this, "MyClusterCDKTest", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.LoadBalancedFargateService(this, "MyFargateServiceCDKTest", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"), // Required
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is false
+    });
+  }
+}
