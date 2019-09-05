746bfc04cef74bf16a4f6b9bcb09517092f6d25c
Ilya Eremin <ilya.eremin@flatstack.com>
2019-09-05 18:15:37 +0300

    push my changes for James Ma

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,9 +3,12 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CdkStack } from '../lib/cdk-stack';
 
+const AWS_REGION=  'us-east-1';
+const AWS_ACCOUNT_ID=  '363852383723';
+
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', { env: {
-        region: 'AWS_REGION',
-        account: 'AWS_ACCOUNT_ID',
+        region: AWS_REGION,
+        account: AWS_ACCOUNT_ID,
     }
 });
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,7 +2,11 @@ import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
 import ecs = require('@aws-cdk/aws-ecs');
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
-import {SecurityGroup} from "@aws-cdk/aws-ec2";
+import { SecurityGroup } from "@aws-cdk/aws-ec2";
+
+const VPC_NAME =  'Twiage Staging';
+const SECURITY_GROUP_ID=  'sg-2f35bc58';
+const CLUSTER_NAME=  'staging-app-servers';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,21 +16,20 @@ export class CdkStack extends cdk.Stack {
         this,
         'VPC',
         {
-          vpcName: 'VPC_NAME'
+          vpcName: VPC_NAME
         },
     );
 
-    const sg = ec2.SecurityGroup.fromSecurityGroupId(this, "SecurityGroup", 'SECURITY_GROUP_ID');
+    const sg = ec2.SecurityGroup.fromSecurityGroupId(this, "SecurityGroup", SECURITY_GROUP_ID);
 
     const cluster = ecs.Cluster.fromClusterAttributes(this,
-        'staging-app-servers', {
-          clusterName: "CLUSTER_NAME",
+        CLUSTER_NAME, {
+          clusterName: CLUSTER_NAME,
           securityGroups: [sg],
           vpc: vpc
         });
 
-    // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.LoadBalancedFargateService(this, "MyFargateServiceCDKTest", {
+    new ecs_patterns.LoadBalancedFargateService(this, "TestCDK", {
       cluster: cluster, // Required
       cpu: 256, // Default is 256
       desiredCount: 1, // Default is 1
